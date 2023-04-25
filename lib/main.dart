5fe3daaaa141a259b9ee493a98183b23485d3691
vaidehi2701 <vaidehi2701@gmail.com>
2023-04-25 16:24:32 +0530

    add main file

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Add To Cart',),
+      home: const MyHomePage(title: 'Add To Cart',),
       debugShowCheckedModeBanner: false,
     );
   }
